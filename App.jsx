a28a500bfea0c1c582358061fc51fa718030c275
Naresh Kumar Budha <52169361+nareshbudha@users.noreply.github.com>
2024-08-10 15:32:15 +0545

    initial

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './Component/Header'
+import Section from './Component/Section'
+import Footer from './Component/Footer'
+
+function App() {
+  return (
+    <div className=''>
+     <Header/>
+     <Section/>
+     <Footer/>
+
+
+
+    </div>
+  )
+}
+
+export default App
